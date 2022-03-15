faba02388a1324f6cf99614a524a0de5b457d5ca
ije90s <idgocm@gmail.com>
2022-03-15 12:58:07 +0900

    3강 완

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -30,4 +30,40 @@ sum = function sum(a: number, b: number): number{
     return a + b;
 }
 
-//git 테스트 
+// 인덱싱
+interface StringArray {
+    [index: number]: string;
+}
+var arr: StringArray = ['a', 'b', 'c'];
+// arr[0] = 10;
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+    [key: string]: RegExp
+}
+
+var obj: StringRegexDictionary = {
+    
+    // sth: /abc/,
+    cssFile: /\.css$/,
+    jsFile:  /\.js$/,
+}
+//obj['cssFile'] = 'a';
+
+Object.keys(obj).forEach(function(value) {});
+
+// 인터페이스 확장(상속)
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface Developer extends Person {
+    language: string;  
+}
+
+var john: Developer = {
+    language: 'ts',
+    name: 'john',
+    age: 33
+}
