e37c4e44846a23acba273a71a7b90ea2f80bd127
ije90s <idgocm@gmail.com>
2022-03-17 12:46:50 +0900

    유니온 타입

---
+++ class-note/5_operator.ts
@@ -0,0 +1,35 @@
+//function logMessage(value: any){
+ //   console.log(value);
+//}
+//logMessage('hello');
+//logMessage(100);
+
+var seho: string | number | boolean;
+function logMessage(value: string | number){
+    if(typeof value === 'number'){
+        value.toLocaleString();
+    }
+
+    if(typeof value === 'string'){
+        value.toString();
+    }
+    throw new TypeError('value must be string or number');
+}
+logMessage("hello");
+logMessage(100);
+
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person{
+    name: string;
+    age: number;
+}
+
+function askSomeone(someone: Developer | Person){
+    someone.name; 
+    someone.skill // 타입을 확인하여 접근 가능하나, 공통된 속성이 아니라서 접근 불가능
+    someone.age
+}
