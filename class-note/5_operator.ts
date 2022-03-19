84774e8015092ba7f75b53aa02b409ec9e4ca560
ije90s <idgocm@gmail.com>
2022-03-19 15:12:13 +0900

    인터섹션

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -28,8 +28,23 @@ interface Person{
     age: number;
 }
 
-function askSomeone(someone: Developer | Person){
+/*function askSomeone(someone: Developer | Person){
+    //someone.name; 
+    //someone.skill // 타입을 확인하여 접근 가능하나, 공통된 속성이 아니라서 접근 불가능
+    //someone.age
+}
+askSomeone({name: '디벨로퍼', skill: '웹 개발'});
+askSomeone({name: '캡틴', age: 100});
+*/
+
+function askSomeone(someone: Developer & Person){
+    someone
     someone.name; 
-    someone.skill // 타입을 확인하여 접근 가능하나, 공통된 속성이 아니라서 접근 불가능
+    someone.skill 
     someone.age
 }
+askSomeone({name: '디벨로퍼', skill: '웹 개발'});
+//askSomeone({name: '캡틴', age: 100});
+
+//var john: string | number | boolean; 
+//var capt: string & number & boolean;
