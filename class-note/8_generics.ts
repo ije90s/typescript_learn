bfc1df052ddd10e13e1bf7f5f80347ea5ee71530
ije90s <idgocm@gmail.com>
2022-03-25 12:53:31 +0900

    제네릭

---
+++ class-note/8_generics.ts
@@ -0,0 +1,40 @@
+//function logText(text) {
+//    console.log(text);
+//    return text; 
+//}
+//logText(10);
+//logText('하이');
+//logText(true);
+
+/*function logText<T>(text: T): T{
+    console.log(text);
+    return text;
+}
+
+logText<string>('하이');
+*/
+
+/*function logText(text: string){
+    console.log(text);
+    text.split('').reverse().join();
+    return text;
+}
+
+function logNumber(num: number){
+    console.log(num);
+    return num; 
+}
+*/
+
+function logText(text: string | number){
+    console.log(text);
+    return text;
+}
+
+const a = logText('a');
+a.split('');
+logText(10); 
+//const num = logNumber(10); 
+//logText(true);
+
+
