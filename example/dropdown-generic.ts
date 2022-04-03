d45839b0ac5eb23832abf4831e2a7100309de5a0
ije90s <idgocm@gmail.com>
2022-04-03 20:37:26 +0900

    제네릭2 복습

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,29 @@
-const emails: {value: string; selected: boolean }[] = [
+//이메일 인터페이스 선언
+interface Email {
+  value: string; 
+  selected: boolean; 
+}
+
+const emails: Email[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts: { value:number, selected: boolean }[] = [
+//상품번호 인터페이스 선언
+interface productNumber {
+  value: number; 
+  selected: boolean;
+};
+
+const numberOfProducts: productNumber[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: { value: number; selected: boolean }) {
+//유니온 타입으로 인자값 지정
+function createDropdownItem(item: Email | productNumber ) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -23,4 +36,9 @@ emails.forEach(function (email) {
   const item = createDropdownItem(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
+});
+
+// 유니온 타입으로 지정했기 때문에 numberOfProducts도 오류 X
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem(product);
 });
