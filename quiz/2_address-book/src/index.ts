64ff908bf5dc47b876b008c658c375241e39e996
ije90s <idgocm@gmail.com>
2022-05-07 00:00:28 +0900

    타입 단언 예제

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -109,4 +109,7 @@ class AddressBook {
   /* ------------------------------------------------ */
 }
 
+const div = document.querySelector('div') as HTMLDivElement;
+div.innerText;
+
 new AddressBook();
