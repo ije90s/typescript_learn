91e5595b4edfada7fd9bc8ae8af700c2f1e951a4
ije90s <idgocm@gmail.com>
2022-05-12 12:46:03 +0900

    Mapped Type 재업로드

--- class-note/14_mapped_type.ts
+++ class-note/14_mapped_type.ts
@@ -1,5 +1,10 @@
 type Heros = 'Hulk' | 'Capt' | 'Thor'; 
 type HeroAges = { [K in Heros]: number }
+const ages: HeroAges = {
+    Hulk: '33',
+    Capt: 100, 
+    Thor: 1000
+}
 
 // for in 반복문 코드 
 // var arr = ['a', 'b', 'c']; 
